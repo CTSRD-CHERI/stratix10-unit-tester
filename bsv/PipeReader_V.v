21fe944e8d9f9b8bac2a7cdccbd0aaed3fd2c885
Simon Moore <simon.moore@cl.cam.ac.uk>
2021-07-14 17:20:27 +0100

    Hack around blocking fgetc

--- bsv/PipeReader_V.v
+++ bsv/PipeReader_V.v
@@ -9,9 +9,11 @@ module PipeReader_V
 
    reg [7:0] 	data;
    reg 		valid;
+   reg [11:0] 	read_slow_down;  // HACK: avoid blocking on $fgetc when other work needs to be done
    
    integer fdr;
    integer c;
+   
    initial begin
       fdr = $fopen("bytepipe-host2hw", "rb");
       valid <= 0;
@@ -22,17 +24,26 @@ module PipeReader_V
 
    always @(posedge CLK)
      if(!RST_N)
-       valid <= 1'b0;
+       begin
+	  valid <= 1'b0;
+	  read_slow_down <= 0;
+       end
      else
        if(valid && EN_GET)
 	 valid <= 1'b0;
        else
 	 begin
-	    c = $fgetc(fdr);
-	    if(c>=0)
+	    read_slow_down <= read_slow_down+1;
+	    if(read_slow_down==0)
 	      begin
-		 data <= c;
-		 valid <= 1'b1;
+		 c = $fgetc(fdr);  // TODO: this appears to be blocking despite the named pipe being setup non-blocking
+		 if(c<0) $finish(0);
+		 if(c>=0)
+		   begin
+		      data <= c;
+		      valid <= 1'b1;
+		   end
 	      end
-	 end
+	 end // else: !if(valid && EN_GET)
+
 endmodule
