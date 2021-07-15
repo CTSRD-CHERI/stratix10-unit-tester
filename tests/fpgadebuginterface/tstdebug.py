ecf9c0336733089a944a1aab854dd537f94e2dd5
Simon Moore <simon.moore@cl.cam.ac.uk>
2021-07-15 19:49:29 +0100

    Provide command-line arguments and tidy the test code into an object.

--- tests/fpgadebuginterface/tstdebug.py
+++ tests/fpgadebuginterface/tstdebug.py
@@ -25,47 +25,66 @@
 # Simple test of FPGADebugInterface
 
 import fpga_debug_interface
+import argparse
 
-dbg = fpga_debug_interface.debug_interface(sim=False)  # True=simulate, False=on FPGA
-error = False
 
-def read_check(idx, expected):
-    r = dbg.read(idx)
-    if(r != expected):
-        print("ERROR: reg[%1d]=%2d but expected %2d" % (idx, r, expected))
-        error = True
-    else:
-        print("reg[%1d] == %2d" % (idx, r))
+class simple_test:
+    def __init__(self, simulation_mode):
+        self.error = False
+        self.dbg = fpga_debug_interface.debug_interface(sim=simulation_mode)  # True=simulate, False=on FPGA
 
-def write_report(idx, data):
-    dbg.write(idx, data)
-    print("reg[%1d] <= %2d" % (idx, data))
+    def read_check(self, idx, expected):
+        r = self.dbg.read(idx)
+        if(r != expected):
+            print("ERROR: reg[%1d]=%2d but expected %2d" % (idx, r, expected))
+            self.error = True
+        else:
+            print("reg[%1d] == %2d" % (idx, r))
+
+    def write_report(self, idx, data):
+        self.dbg.write(idx, data)
+        print("reg[%1d] <= %2d" % (idx, data))
     
-def run_simple_test():
-    dbg.clear()
-    write_report(0,3)
-    write_report(2,7)
-    write_report(3,13)
-    write_report(1,17)
-    read_check(0,3)
-    read_check(2,7)
-    read_check(3,13)
-    read_check(1,17)
-    write_report(4,3)
-    write_report(5,3)
-    write_report(6,3)
-    write_report(7,3)
-    read_check(0,6)
-    read_check(1,20)
-    read_check(2,10)
-    read_check(3,16)
-    dbg.end_simulation()
+    def run_test(self):
+        self.dbg.clear()
+        self.write_report(0,3)
+        self.write_report(2,7)
+        self.write_report(3,13)
+        self.write_report(1,17)
+        self.read_check(0,3)
+        self.read_check(2,7)
+        self.read_check(3,13)
+        self.read_check(1,17)
+        self.write_report(4,3)
+        self.write_report(5,3)
+        self.write_report(6,3)
+        self.write_report(7,3)
+        self.read_check(0,6)
+        self.read_check(1,20)
+        self.read_check(2,10)
+        self.read_check(3,16)
+        self.dbg.end_simulation()
     
 if __name__ == "__main__":
-    run_simple_test()
-    if(error):
-        print("Test result: FAIL")
-    else:
-        print("Test result: PASS")
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--fpga', help='test on FPGA', action="store_true")
+    group.add_argument('--sim', help='test in simulation (Icarus Verilog)', action="store_true")
+    parser.add_argument('--n', help='number of iterations', type=int, default=1)
+    args = parser.parse_args()
+    if not((args.fpga and not(args.sim)) or (not(args.fpga) and args.sim)):
+        parser.error('Select --fpga or --sim')
+    if(args.fpga):
+        print("FPGA test starting for %d iterations" % (args.n))
+    if(args.sim):
+        print("Simulation starting for %d iterations" % (args.n))
+    test = simple_test(args.sim)
+    for j in range(args.n):
+        test.run_test()
+        if(test.error):
+            print("Test %d result: FAIL" % (j))
+            exit(-1)
+        else:
+            print("Test %d result: PASS" % (j))
     
 exit(0)
