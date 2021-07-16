4e71b23fd332960c77fb8306974ab2447ce174b7
Simon Moore <simon.moore@cl.cam.ac.uk>
2021-07-16 14:54:16 +0100

    Correct the license

--- bsv/PipeReader_V.v
+++ bsv/PipeReader_V.v
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2021 Simon W. Moore
+ * All rights reserved.
+ *
+ * This software was developed at the University of Cambridge Computer
+ * Laboratory (Department of Computer Science and Technology) based
+ * upon work supported by the DoD Information Analysis Center Program
+ * Management Office (DoD IAC PMO), sponsored by the Defense
+ * Technical Information Center (DTIC) under Contract No. FA807518D0004.
+ *
+ * @BERI_LICENSE_HEADER_START@
+ *
+ * Licensed to BERI Open Systems C.I.C. (BERI) under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  BERI licenses this
+ * file to you under the BERI Hardware-Software License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *   http://www.beri-open-systems.org/legal/license-1-0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, Work distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * @BERI_LICENSE_HEADER_END@
+ * /
 module PipeReader_V
   (
    input 	CLK,
