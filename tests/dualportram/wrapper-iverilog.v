cf9c6492b4b26d0c347c47eeab01d8cd1d0e13e0
Simon Moore <simon.moore@cl.cam.ac.uk>
2022-07-08 14:14:04 +0100

    Initial.

---
+++ tests/dualportram/wrapper-iverilog.v
@@ -0,0 +1,20 @@
+// Trivial iVerilog top-level to provide clock and reset
+module sim(output CLK, output RST_N);
+   reg CLK;
+   reg RST_N;
+
+   top dut(CLK,RST_N);
+
+   initial begin
+      CLK <= 1'b0;
+      RST_N <= 1'b0;
+      #20 RST_N <= 1'b1;
+   end
+
+   always #5 CLK <= !CLK;
+endmodule
+
+   
+   
+
+
