23e36645e4c363f865245da1d3bbae12e226e464
Simon Moore <simon.moore@cl.cam.ac.uk>
2022-08-16 19:41:15 +0000

    Initial

---
+++ py/progallde10pro.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import subprocess as sp
+import sys, os, time
+import re, argparse
+
+default_num_fpgas = 8
+quartus_pgm_timeout = 60
+
+def find_de10pro_devices():
+    devices = {}
+    dev = None
+    jtagchain = []
+    try:
+        proc = sp.run(["jtagconfig"], stdout=sp.PIPE, stderr=sys.stderr, timeout=5, check=True)
+    except:
+        return {}
+    proc_stdout = proc.stdout.decode()
+    for line in proc_stdout.split('\n'):
+        if(dev==None):
+            d = re.match("^(\d+)[\)]\WDE10-Pro\W(\S+)", line)
+            if(d != None):
+                dev = "DE10-Pro "+d.group(2)
+        else:
+            jtag = re.match("^\W+(\S+)\W+([\w\(\)\/\.\|]+)", line)
+            if(jtag==None):  # end of jtag chain
+                devices[dev] = jtagchain
+                dev = None
+                jtagchain = []
+            else:
+                jtagchain.append(jtag.groups())
+    return devices
+
+def spawn_quartus_pgm(devices,sof,sequential):
+    process_list = []
+    no_license_env = os.environ.copy()
+    no_license_env['LM_LICENSE_FILE']=''
+    for d in devices.keys():
+        print("usb-to-jtag=", d, " jtag =", devices[d])
+        id = 0
+        j=0
+        for chain in devices[d]:
+            j=j+1
+            if(re.match("1SX280HH1",chain[1])):
+                id=j
+        cmd = ['quartus_pgm', '-m', 'jtag', '-c', d, '-o', 'p;%s@%d'%(sof,id)]
+        print(' '.join(cmd))
+        p=sp.Popen(cmd, stdout=sp.PIPE, stderr=sys.stderr, env=no_license_env)
+        process_list.append(p)
+        if(sequential):
+            p.wait(timeout=quartus_pgm_timeout)
+    return process_list
+
+def report_process_status(devices, process_list):
+    for d in devices.keys():
+        proc = process_list.pop(0)
+        try:
+            proc.wait(timeout=quartus_pgm_timeout)
+        except:
+            print("%s: ERROR: Process programming timed out"%(d))
+            proc.kill()
+        else:
+            report=[]
+            so = proc.communicate()[0].decode()
+            for l in so.split('\n'):
+                if(   re.match("Info: Quartus Prime Programmer was", l)
+                   or re.match("\W+Info: Elapsed time", l)
+                   or re.match("\W+Error", l)):
+                    report.append(d+": "+l)
+            print('\n'.join(report))
+
+def main():
+    global quartus_pgm_timeout
+    parser = argparse.ArgumentParser(prog='progallde10pro.py',
+                                     description='Program all DE10Pro FPGA boards')
+    parser.add_argument('-n', '--numfpga', type=int, action='store', default=default_num_fpgas,
+                        help='number of FPGAs in the system (default: %d)'%(default_num_fpgas))
+    parser.add_argument('sof', type=str, action='store',
+                        help='SOF file to program the FPGA')
+    parser.add_argument('-s', '--sequential', action='store_true', default=False,
+                        help='program FPGAs sequentially')
+    parser.add_argument('-t', '--timeout', type=int, action='store', default=quartus_pgm_timeout,
+                        help='quartus_pgm timeout in seconds (default=%ds)'%(quartus_pgm_timeout))
+    args = parser.parse_args()
+    quartus_pgm_timeout = args.timeout
+    if(not(os.path.exists(args.sof))):
+        print("SOF file %s does not exist"%(args.sof))
+        return(1)
+
+    devices = []
+    timeout = 4
+    while((len(devices)!=args.numfpga) and (timeout>0)):
+        devices = find_de10pro_devices()
+        timeout = timeout-1
+    if(timeout==0):
+        print("Found %d FPGAs but you asked to program %d. Exiting."%(len(devices),args.numfpga))
+        return(1)
+    if(args.sequential):
+        print("Programming sequentially")
+    process_list = spawn_quartus_pgm(devices=devices,sof=args.sof,sequential=args.sequential)
+    report_process_status(devices, process_list)
+
+    
+if __name__ == '__main__':
+    start = time.time()
+    status = main()
+    end = time.time()
+    print("Real time: %2.3fs"%(end-start))
+    sys.exit(status)
